1d850288bf0831b51df0313d6b116f698732fe87
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-01 20:11:23 +0500

    refactor(backend): create files for auth, users routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,11 +5,21 @@ import { jwt, type JwtVariables } from 'hono/jwt';
 import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
 
+import auth from './routes/auth';
+import users from './routes/users';
+
 const app = new Hono<{ Variables: JwtVariables }>();
 
 app.use(logger());
 app.use('/v1/*', cors());
-app.use('/v1/*', jwt({ secret: process.env.JWT_SECRET!, alg: 'HS256' }));
+app.use(
+  '/v1/*',
+  jwt({
+    secret: process.env.JWT_SECRET!,
+    alg: 'HS256',
+    cookie: 'token',
+  }),
+);
 
 app.get(
   '/openapi',
@@ -20,9 +30,18 @@ app.get(
         version: '1.0.0',
         description: 'The simplest link shortener & social landing hosting',
       },
+      servers: [
+        {
+          url: 'https://api.balabol.cc/v1',
+          description: 'Production',
+        },
+      ],
     },
   }),
 );
 app.get('/docs', Scalar({ url: '/openapi' }));
+
+app.route('/auth', auth);
+app.route('/users', users);
 
 export default app;
