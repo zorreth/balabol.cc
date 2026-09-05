8c32cb04fa3249e3439d9bc59939ff038e81a17f
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-06 03:57:37 +0500

    feat(backend): add more validation rules for the user routes

--- apps/backend/src/routes/users.ts
+++ apps/backend/src/routes/users.ts
@@ -6,22 +6,33 @@ import { eq } from 'drizzle-orm';
 import { describeRoute, resolver, validator } from 'hono-openapi';
 import * as v from 'valibot';
 import { jwt } from 'hono/jwt';
-import { bearerAuth } from 'hono/bearer-auth';
 
-const userSchema = v.object({
-  username: v.string(),
-  displayName: v.string(),
+const UsernameSchema = v.pipe(
+  v.string(),
+  v.minLength(3, 'The minimum username length is 3 characters.'),
+  v.maxLength(64, 'The maximum username length is 64 characters.'),
+  v.regex(/^[a-zA-Z0-9]+$/, 'The username must be alphanumeric.'),
+);
+
+const DisplayNameSchema = v.pipe(
+  v.string(),
+  v.maxLength(64, 'The maximum display name length is 64 characters.'),
+);
+
+const UserSchema = v.object({
+  username: UsernameSchema,
+  displayName: DisplayNameSchema,
   bio: v.string(),
   avatarUrl: v.string(),
 });
 
-const userUpdateSchema = v.object({
-  username: v.optional(v.string()),
-  displayName: v.optional(v.string()),
+const UserUpdateSchema = v.object({
+  username: v.optional(UsernameSchema),
+  displayName: v.optional(DisplayNameSchema),
   bio: v.optional(v.string()),
 });
 
-const errorSchema = v.object({
+const ErrorSchema = v.object({
   status: v.number(),
   message: v.string(),
 });
@@ -36,13 +47,13 @@ app.get(
       200: {
         description: 'Successfully retrieved user',
         content: {
-          'application/json': { schema: resolver(userSchema) },
+          'application/json': { schema: resolver(UserSchema) },
         },
       },
       404: {
         description: 'User not found',
         content: {
-          'application/json': { schema: resolver(errorSchema) },
+          'application/json': { schema: resolver(ErrorSchema) },
         },
       },
     },
@@ -78,25 +89,25 @@ app.patch(
       200: {
         description: 'Successfully updated user',
         content: {
-          'application/json': { schema: resolver(userSchema) },
+          'application/json': { schema: resolver(UserSchema) },
         },
       },
       401: {
         description: 'Unauthorized',
         content: {
-          'application/json': { schema: resolver(errorSchema) },
+          'application/json': { schema: resolver(ErrorSchema) },
         },
       },
       404: {
         description: 'User not found',
         content: {
-          'application/json': { schema: resolver(errorSchema) },
+          'application/json': { schema: resolver(ErrorSchema) },
         },
       },
     },
   }),
   jwt({ secret: process.env.JWT_SECRET!, cookie: 'token', alg: 'HS256' }),
-  validator('json', userUpdateSchema),
+  validator('json', UserUpdateSchema),
   async (c) => {
     const payload = c.get('jwtPayload') as { sub: number };
     const userId = payload.sub;
