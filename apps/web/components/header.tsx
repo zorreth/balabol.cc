ce07e1843a15501fe8cc05ba6b4a1abc6c48bef2
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-10 13:41:47 +0500

    refactor(web): make header responsive

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Button, buttonVariants } from './ui/button';
+import { MenuIcon } from 'lucide-react';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
 const navItems = [
   {
@@ -24,13 +26,14 @@ export function Header() {
         <Image
           src="/logo.svg"
           alt="logo"
-          className="hover:scale-105 transition"
+          className="hover:scale-105 transition w-36 h-auto"
+          loading="eager"
           width={140}
           height={80}
         />
       </Link>
 
-      <div>
+      <div className="hidden md:flex">
         {navItems.map((item) => (
           <Link
             key={item.href}
@@ -42,7 +45,35 @@ export function Header() {
         ))}
       </div>
 
-      <Button size="lg">Sign In</Button>
+      <Button className="hidden md:flex" size="lg">
+        Sign In
+      </Button>
+
+      <Sheet>
+        <SheetTrigger
+          render={<Button size="icon-lg" variant="ghost" className="md:hidden" />}
+        >
+          <MenuIcon />
+        </SheetTrigger>
+
+        <SheetContent showCloseButton={true}>
+          <div className="flex flex-col gap-2 p-4">
+            <div className="flex flex-col items-start">
+              {navItems.map((item) => (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={buttonVariants({ variant: 'link', size: 'lg' })}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+
+            <Button size="lg">Sign In</Button>
+          </div>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 }
