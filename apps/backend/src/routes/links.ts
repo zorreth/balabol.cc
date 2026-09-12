df61ca2d1000947f6dadf26fc4c42455ec3eed4f
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-12 20:48:06 +0500

    feat(backend): add endpoint to delete user link

--- apps/backend/src/routes/links.ts
+++ apps/backend/src/routes/links.ts
@@ -5,13 +5,14 @@ import { db } from '../db';
 import { links } from '../db/schema';
 import { HTTPException } from 'hono/http-exception';
 import { ErrorSchema, LinkCreateSchema, LinkSchema } from '@repo/schemas';
+import { and, eq } from 'drizzle-orm';
 
 const app = new Hono();
 
 app.post(
   '/',
   describeRoute({
-    description: 'Create a new link for the current user',
+    description: 'Create a new link',
     security: [{ cookieAuth: [] }, { bearerAuth: [] }],
     responses: {
       201: {
@@ -61,6 +62,42 @@ app.post(
       console.error(error);
       throw new HTTPException(500, { message: 'Internal Server Error' });
     }
+  },
+);
+
+app.delete(
+  '/:id',
+  describeRoute({
+    description: 'Delete link by id',
+    responses: {
+      204: {
+        description: 'Successfully deleted link',
+      },
+      404: {
+        description: 'Link not found',
+        content: {
+          'application/json': { schema: resolver(ErrorSchema) },
+        },
+      },
+    },
+  }),
+  jwt({ secret: process.env.JWT_SECRET!, cookie: 'token', alg: 'HS256' }),
+  async (c) => {
+    const payload = c.get('jwtPayload') as { sub: number };
+    const userId = payload.sub;
+
+    const linkId = c.req.param('id');
+
+    const deletedRows = await db
+      .delete(links)
+      .where(and(eq(links.id, Number(linkId)), eq(links.userId, userId)))
+      .returning();
+
+    if (!deletedRows.length) {
+      throw new HTTPException(404, { message: 'Link not found' });
+    }
+
+    return c.body(null, 204);
   },
 );
 
