becdfb820682510d2e46e36d21afc7ab5eead96c
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-13 19:19:07 +0500

    fix(web): add loading=eager to the cat error gif

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -22,7 +22,13 @@ export default function NotFound() {
         if you think there is a mistake.
       </p>
 
-      <Image src="/cat-error.gif" alt="cat error" width={498} height={328} />
+      <Image
+        src="/cat-error.gif"
+        alt="cat error"
+        loading="eager"
+        width={498}
+        height={328}
+      />
     </div>
   );
 }
