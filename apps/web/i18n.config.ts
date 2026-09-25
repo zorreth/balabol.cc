8cd74330fbf70b9c18227f7f9a465fc491fe08b9
Kirill Siukhin <kirillsiukhin@proton.me>
2026-09-26 02:24:13 +0500

    i18n: add brand page translations

--- apps/web/i18n.config.ts
+++ apps/web/i18n.config.ts
@@ -3,7 +3,7 @@ import type { I18nConfig } from 'next-i18next/proxy';
 const i18nConfig: I18nConfig = {
   supportedLngs: ['en', 'ru'],
   fallbackLng: 'en',
-  ns: ['common', 'header', 'footer'],
+  ns: ['common', 'header', 'footer', 'brand'],
   defaultNS: 'common',
   localeInPath: false,
   resourceLoader: (language, namespace) =>
